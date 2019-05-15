88a0b984c3398b5a3a37419647638bd17808793a
atayodele <atayodele555@gmail.com>
2019-05-15 14:19:35 +0100

    change proj to AuthorQuiz

---
+++ src/AuthorQuiz.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './bootstrap.min.css';
+
+
+class AuthorQuiz extends Component{
+  render() {
+    return (
+      <div>
+        <h1>AuthorQuiz</h1>
+      </div>
+    )
+  }
+}
+
+export default AuthorQuiz;
